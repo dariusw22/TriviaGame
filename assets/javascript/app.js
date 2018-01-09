47a13323b690aafa7b3a0bf6ffaef649e3a70b3f
dariusw22.dw@gmail.com <dariuswilliams@dariuss-mbp.attlocal.net>
2018-01-09 14:19:03 -0500

    adding the work after the timer is finished for now

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(".questionContainer").hide();
+	$(".next").hide();
+	$(".score")
+});
+
+
+
+//function that holds the timer. 
+//code that hides the questions until the start button is pressed.
+//code that holds the score.
